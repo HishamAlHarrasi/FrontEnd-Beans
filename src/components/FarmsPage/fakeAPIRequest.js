96ddecafc9cfe1b7d1ee33b249afbf52bdfc61f7
HishamAlHarrasi <u1840156@live.warwick.ac.uk>
2021-02-19 18:55:05 +0400

    Fixed validation on AddUser page, and other minor fixes

---
+++ src/components/FarmsPage/fakeAPIRequest.js
@@ -0,0 +1,16 @@
+export default function databaseRequest2() {
+    return (
+        [
+            {
+                farmName: "Farm A",
+                farmLocation: "Algeria",
+                tunnels: [
+                    {
+                        id: 1,
+                        
+                    }
+                ]
+            }
+        ]
+    )
+}

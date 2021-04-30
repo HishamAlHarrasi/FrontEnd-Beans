108102b861815b5844f60cefd661e1dec52872f5
Hisham <u1840156@live.warwick.ac.uk>
2021-04-30 05:59:45 +0400

    Password reset fixed and connected with api

--- src/components/HomePage/HomePage.jsx/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx/HomePage.jsx
@@ -2,12 +2,18 @@ import axios from "axios";
 import jwtDecode from "jwt-decode";
 import React, { Component } from "react";
 import checkJWT from "../../shared/checkJWT";
+import { toast } from "react-toastify";
 
 let token = "";
 let config = {};
 
 class HomePage extends Component {
-  state = { userData: {}, resetPasswordClicked: false };
+  state = {
+    userData: {},
+    resetPasswordClicked: false,
+    passwordResetToken: "",
+    errors: "",
+  };
 
   async componentDidMount() {
     await checkJWT();
@@ -24,7 +30,62 @@ class HomePage extends Component {
     console.log(jwtDecode(token));
   }
 
+  resetPassword = async (event) => {
+    event.preventDefault();
+
+    let errors = this.state.errors;
+
+    let password = event.target[0].value;
+    let password2 = event.target[1].value;
+
+    if (password.trim() === "") {
+      errors = "Password is required.";
+      this.setState({ errors: errors });
+      return;
+    } else if (password.length <= 6) {
+      errors = "Password must be at least 8 characters.";
+      this.setState({ errors: errors });
+      return;
+    }
+    if (password2.trim() === "") {
+      errors = "Password confirmation is required.";
+      this.setState({ errors: errors });
+      return;
+    } else if (password !== password2) {
+      errors = "Passwords are not the same.";
+      this.setState({ errors: errors });
+      return;
+    } else {
+      errors = "";
+      this.setState({ errors: errors });
+    }
+
+    await axios
+      .get(
+        "http://" + process.env.REACT_APP_server + "/api/users/password/reset",
+        config
+      )
+      .then((resp) =>
+        this.setState({ passwordResetToken: resp.data.reset_token })
+      )
+      .catch((err) => console.log(err));
+
+    await axios
+      .post(
+        "http://" + process.env.REACT_APP_server + "/api/users/password",
+        { token: this.state.passwordResetToken, password: password },
+        config
+      )
+      .then((resp) => {
+        toast.success("Password reset successfully.");
+        this.setState({ resetPasswordClicked: false });
+      })
+      .catch((err) => toast.error("Password reset failed."));
+  };
+
   render() {
+    const { errors } = this.state;
+
     return (
       <div className="container">
         <h3 style={{ marginTop: "20px" }}>Personal Information</h3>
@@ -63,29 +124,44 @@ class HomePage extends Component {
             </button>
             {this.state.resetPasswordClicked ? (
               <div className="home-page-pw-rst">
-                <br />
-                <label className="form-label lbl">First Name: </label>
-                <input
-                  id="firstname"
-                  type="text"
-                  className="form-input-pw-reset"
-                  placeholder=" Password here.."
-                />
-                <br />
-                <br />
-
-                <label className="form-label lbl">First Name: </label>
-                <input
-                  id="firstname"
-                  type="text"
-                  className="form-input-pw-reset"
-                  placeholder=" Confirm password.."
-                />
-                <br />
-                <br />
-                <button className="btn btn-primary btn-sm pw-reset-submit">
-                  Change Password
-                </button>
+                <form
+                  onSubmit={(event) => {
+                    this.resetPassword(event);
+                  }}
+                >
+                  <br />
+                  <label className="form-label lbl">New Password: </label>
+                  <input
+                    id="firstname"
+                    type="password"
+                    className="form-input-pw-reset"
+                    placeholder=" New Password here.."
+                  />
+                  <br />
+                  <br />
+                  <label className="form-label lbl">
+                    Confirm New Password:
+                  </label>
+                  <input
+                    id="firstname"
+                    type="password"
+                    className="form-input-pw-reset"
+                    placeholder=" Confirm password.."
+                  />
+                  <br />
+                  <br />
+                  {errors && (
+                    <div className="alert alert-danger error-label-pw-reset">
+                      {errors}
+                    </div>
+                  )}
+                  <button
+                    className="btn btn-primary btn-sm pw-reset-submit"
+                    type="submit"
+                  >
+                    Change Password
+                  </button>
+                </form>
               </div>
             ) : (
               <div></div>
